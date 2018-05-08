05e1001f8281b6a65ee5bfd42a2473281cdf254c
syeutyu <syeuty@naver.com>
2018-05-08 15:45:44 +0900

    [Update] 좀더 좋은 키위주스 알고리즘

--- src/KiwiJuice/Main.java
+++ src/KiwiJuice/Main.java
@@ -7,10 +7,10 @@ public class Main {
     public static int[] thePouring(int[] capacities, int[] bottles, int[] fromId, int[] toId) {
         for (int i = 0; i < fromId.length; i++) {
             int sum = bottles[fromId[i]] + bottles[toId[i]];
-            if(capacities[toId[i]] < sum){
+            if (capacities[toId[i]] < sum) {
                 bottles[fromId[i]] = sum - capacities[toId[i]];
                 bottles[toId[i]] = capacities[toId[i]];
-            } else{
+            } else {
                 bottles[fromId[i]] = 0;
                 bottles[toId[i]] = sum;
             }
@@ -18,11 +18,20 @@ public class Main {
         return bottles;
     }
 
+    public static int[] refactorThePouring(int[] capacities, int[] bottles, int[] fromId, int[] toId) {
+        for (int i = 0; i < fromId.length; i++) {
+            int sum = bottles[fromId[i]] + bottles[toId[i]];
+            bottles[toId[i]] = Math.min(sum,capacities[toId[i]]);
+            bottles[fromId[i]] = sum - bottles[toId[i]];
+        }
+        return bottles;
+    }
+
     public static void main(String args[]) {
-        System.out.println(Arrays.toString(thePouring(new int[]{20, 20}, new int[]{5, 8}, new int[]{0}, new int[]{1})));
-        System.out.println(Arrays.toString(thePouring(new int[]{10, 10}, new int[]{5, 8}, new int[]{0}, new int[]{1})));
-        System.out.println(Arrays.toString(thePouring(new int[]{30,20, 10}, new int[]{10,5,5}, new int[]{0,1,2}, new int[]{1,2,0})));
-        System.out.println(Arrays.toString(thePouring(new int[]{14, 35,86,58,25,62}, new int[]{6,34,27,38,9,60}, new int[]{1,2,4,5,3,3,1,0}, new int[]{0,1,2,4,2,5,3,1})));
+        System.out.println(Arrays.toString(refactorThePouring(new int[]{20, 20}, new int[]{5, 8}, new int[]{0}, new int[]{1})));
+        System.out.println(Arrays.toString(refactorThePouring(new int[]{10, 10}, new int[]{5, 8}, new int[]{0}, new int[]{1})));
+        System.out.println(Arrays.toString(refactorThePouring(new int[]{30, 20, 10}, new int[]{10, 5, 5}, new int[]{0, 1, 2}, new int[]{1, 2, 0})));
+        System.out.println(Arrays.toString(refactorThePouring(new int[]{14, 35, 86, 58, 25, 62}, new int[]{6, 34, 27, 38, 9, 60}, new int[]{1, 2, 4, 5, 3, 3, 1, 0}, new int[]{0, 1, 2, 4, 2, 5, 3, 1})));
     }
 
 }
