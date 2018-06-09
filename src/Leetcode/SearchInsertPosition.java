fe9d78882444cc48954591a49f2a47fbf2fd93e0
syeutyu <syeuty@naver.com>
2018-06-09 11:54:09 +0900

    [ADD] Array에 Value Insert Index 찾기 알고리즘

---
+++ src/Leetcode/SearchInsertPosition.java
@@ -0,0 +1,15 @@
+package Leetcode;
+
+public class SearchInsertPosition {
+
+    public static int searchInsert(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if(target<=nums[i]) return i;
+        }
+        return nums.length;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(searchInsert(new int[]{1, 3, 5, 6}, 2));
+    }
+}
