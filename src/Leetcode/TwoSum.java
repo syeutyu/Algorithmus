141dfdc9d78d9732e35392355c820bda4bbea5c9
syeutyu <syeuty@naver.com>
2018-06-16 22:31:11 +0900

    HashTable을 이용한 시간복잡도 절약

--- src/Leetcode/TwoSum.java
+++ src/Leetcode/TwoSum.java
@@ -1,13 +1,15 @@
 package Leetcode;
 
 import java.util.Arrays;
+import java.util.Hashtable;
+import java.util.Map;
 
 public class TwoSum {
     public static int[] twoSum(int[] nums, int target) {
         int[] arr = new int[2];
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
-                if(nums[i] + nums[j] == target){
+                if (nums[i] + nums[j] == target) {
                     arr[0] = i;
                     arr[1] = j;
                 }
@@ -16,8 +18,24 @@ public class TwoSum {
         return arr;
     }
 
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 15}, 9)));
+    static int[] hashTable(int[] nums, int target) throws Exception {
+        Map<Integer, Integer> map = new Hashtable<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int temp = target - nums[i];
+
+            if (map.containsKey(temp)) {
+                return new int[]{map.get(temp),i};
+            }
+            map.put(nums[i], i);
+
+        }
+        throw new Exception("Not found Target");
+        }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 15}, 17)));
+        System.out.println(Arrays.toString(hashTable(new int[]{2, 7, 11, 15}, 9)));
     }
 }
 
